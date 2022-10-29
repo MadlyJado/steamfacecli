f6e7b409d51f665a7cea13b758f5508e55cb2482
MadlyJado <jadonchenard@gmail.com>
2022-10-29 14:04:56 -0400

    Can now print amount of faces to console!

--- steamfacecli.py
+++ steamfacecli.py
@@ -1,7 +1,9 @@
+from queue import Empty
 from tkinter import *
 from tkinter import ttk
 import cv2
 import sys
+import numpy
 
 class FacialRecognition():
     # Get user supplied images
@@ -10,36 +12,44 @@ class FacialRecognition():
 
     # Create the haar cascade
     faceCascade = cv2.CascadeClassifier(cascPath)
-    video_capture = cv2.VideoCapture(0)
-
-    while True:
-        # Capture webcam frame by frame
-        ret, frame = video_capture.read()
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
-        
-        # Display the resulting frame
-        cv2.imshow('Video', frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    def AmountOfFaces(self):
-        Faces = self.faces
-        print("Found {0} faces!".format(len(Faces)))
     
-    # When everything is done, release the capture
-    video_capture.release()
-    cv2.destroyAllWindows()
+    faces = []
+
+    def GetFace(self):
+        video_capture = cv2.VideoCapture(0)
+        while True:
+
+            # Capture webcam frame by frame
+            ret, frame = video_capture.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            face = self.faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        # When everything is done, release the capture
+        self.faces = face
+        video_capture.release()
+        cv2.destroyAllWindows()
+
+    # Return the amount of faces found by the facial recognition cascade
+    def __str__(self):
+        global faces
+        Faces = self.faces
+        return "Found {0} faces!".format(len(Faces))
+    
+    
    
 
 facialrecognition = FacialRecognition()
 
-facialrecognition.AmountOfFaces()
+facialrecognition.GetFace()
+print(facialrecognition)
