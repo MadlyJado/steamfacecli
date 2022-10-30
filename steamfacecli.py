a6b11b59c371a9add3b42e4d3074217a21dbd8d3
MadlyJado <jadonchenard@gmail.com>
2022-10-29 20:39:32 -0400

    Created the process for signing up for steamface

--- steamfacecli.py
+++ steamfacecli.py
@@ -1,7 +1,11 @@
 import cv2
 import sys
 import numpy
-import hashlib
+import maskpass
+import random
+from hashlib import sha256
+import os
+import pickle
 
 class FacialRecognition():
     # Get user supplied images
@@ -13,7 +17,35 @@ class FacialRecognition():
     
     faces = []
 
-    def GetFace(self):
+    def SaveFace(self):
+        video_capture = cv2.VideoCapture(0)
+        while True:
+
+            # Capture webcam frame by frame
+            ret, frame = video_capture.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            face = self.faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+            # Display the resulting frame
+            cv2.imshow('Video', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                path = './known/'+str(random.getrandbits(10))+'.png'
+                cv2.imwrite(path, gray)
+                break
+        # When everything is done, release the capture
+        self.faces = face
+        video_capture.release()
+        cv2.destroyAllWindows()
+        return path
+    
+    def ScanFace(self):
         video_capture = cv2.VideoCapture(0)
         while True:
 
@@ -49,5 +81,44 @@ class FacialRecognition():
 
 facialrecognition = FacialRecognition()
 
-facialrecognition.GetFace()
+facialrecognition.SaveFace()
 print(facialrecognition)
+
+class SteamFace():
+    
+    usernames = {
+        
+    }
+    
+    passwords = {
+        
+    }
+    facialRecognition = FacialRecognition()
+    
+    def register(self, user, password):
+        # Create salt to make password authentication truly secure
+        salt = str(random.randbytes(10))
+        # Ask user to enter their Steam username
+        user = input("Enter your steam username: ")
+        # Ask user to enter their Steam password
+        password = maskpass.askpass(prompt= "Enter the password to your steam account: ", mask="#")
+        # Hash it using the salt and password
+        hashedPassword = sha256(password+salt)
+        # Get the user's face image path by saving their face.
+        imagepath = self.facialRecognition.SaveFace()
+        # Save username to dictionary linking the username to the image path
+        self.usernames[user] = imagepath
+        # Do the same with the hashed password
+        self.passwords[hashedPassword] = imagepath
+        # Save the dictionaries locally to the root folder of the program
+        pickle.dump(self.usernames, './usernames.pkl')
+        pickle.dump(self.passwords, './passwords.pkl')
+        
+        
+        
+        
+        
+        
+        
+        
+    
